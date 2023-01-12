f8a3281f67af2c2bf397a21c65e61569da884c64
SpAcE Swings <ATZ_21@mail.ru>
2023-01-12 18:45:56 +0300

    12.01

--- src/main/java/by/ivankov/task3/file/impl/ArrayFileReaderServiceImpl.java
+++ src/main/java/by/ivankov/task3/file/impl/ArrayFileReaderServiceImpl.java
@@ -16,26 +16,27 @@ import java.util.Scanner;
 
 public class ArrayFileReaderServiceImpl implements ArrayFileReaderService {
     static Logger logger = LogManager.getLogger();
-    private static final String DEFAULT_FILENAME = "data//arrayFile.txt";
+    private static final String FILENAME = "data//arrayFile.txt";
 
     public CustomArray arrayReader(String fileName) {
-        CustomArray customArray = new CustomArray();
-        int[] arr = null;
         Path path = Path.of(fileName);
         if (!Files.exists(path)) {
             logger.log(Level.ERROR, "file" + fileName + " not exist");
-            fileName = DEFAULT_FILENAME;
+            fileName = FILENAME;
         }
+        CustomArray customArray = new CustomArray();
+        int[] arr = null;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            Scanner scanner = new Scanner(reader);
-            String tmp = scanner.nextLine();
-            String[] str = tmp.split("\\s+");
-            arr = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                try {
-                    arr[i] = Integer.parseInt(str[i]);
-                } catch (NumberFormatException e) {
-                    logger.log(Level.ERROR, "Invalid data type");
+            String tmp;
+            if ((tmp = reader.readLine()) != null) {
+                String[] str = tmp.split("\\s+");
+                arr = new int[str.length];
+                for (int i = 0; i < str.length; i++) {
+                    try {
+                        arr[i] = Integer.parseInt(str[i]);
+                    } catch (NumberFormatException e) {
+                        logger.log(Level.ERROR, "Invalid data type");
+                    }
                 }
             }
             customArray.setArray(arr);
