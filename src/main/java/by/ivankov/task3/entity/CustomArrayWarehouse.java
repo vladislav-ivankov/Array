1cbe88f825b3c5ad6ffebbf3a57a583fb0eb5535
SpAcE Swings <ATZ_21@mail.ru>
2023-01-30 18:41:56 +0300

    30.01

--- src/main/java/by/ivankov/task3/entity/CustomArrayWarehouse.java
+++ src/main/java/by/ivankov/task3/entity/CustomArrayWarehouse.java
@@ -1,23 +1,4 @@
 package by.ivankov.task3.entity;
 
-import java.util.*;
-
 public class CustomArrayWarehouse {
-    static final CustomArrayWarehouse INSTANCE = new CustomArrayWarehouse();
-    Map<Integer, IntSummaryStatistics> content = new HashMap<>();
-
-    static CustomArrayWarehouse getInstance() {
-        return INSTANCE;
-    }
-
-    public void summaryStatistics(List<CustomArray> arrays) {
-        content.clear();
-        for (CustomArray i : arrays) {
-            content.put(i.getArrayId(), Arrays.stream(i.getArray()).summaryStatistics());
-        }
-    }
-
-    public String getSummaryStatistics() {
-        return content.toString();
-    }
 }
